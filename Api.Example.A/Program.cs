f8e46ab99964f938fb4047ce356dbae30ae9a88d
ercilli <eric.tarelli@gmail.com>
2024-05-26 16:55:09 -0300

    subir

--- Api.Example.A/Program.cs
+++ Api.Example.A/Program.cs
@@ -12,7 +12,6 @@ builder.Services.AddSwaggerGen();
 //builder.Services.AddPaaS(configuration);
 builder.Services.AddPOM(configuration);
 
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
