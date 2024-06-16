6ee22f02e4b82112f4573ffeabc361ca96140465
ercilli <eric.tarelli@gmail.com>
2024-06-15 21:29:40 -0300

    Refactor TraceIdEnricher to use Activity.Current for accessing trace and span IDs

---
+++ Logging.Configuration/Enrichers/TraceIdEnricher.cs
@@ -0,0 +1,18 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+using System.Diagnostics;
+
+public class TraceIdEnricher : ILogEventEnricher
+{
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+    {
+        var activity = Activity.Current;
+        if (activity != null)
+        {
+            var traceId = activity.TraceId.ToString();
+            var spanId = activity.SpanId.ToString();
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("trace_id", traceId));
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("span_id", spanId));
+        }
+    }
+}
