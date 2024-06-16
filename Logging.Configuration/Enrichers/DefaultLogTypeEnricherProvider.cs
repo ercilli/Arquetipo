80b24a9800270c5df2cc48c79cc1c5684539773c
ercilli <eric.tarelli@gmail.com>
2024-06-15 21:29:52 -0300

    Refactor DefaultLogTypeEnricherProvider to use IHttpContextAccessor for accessing HttpContext

---
+++ Logging.Configuration/Enrichers/DefaultLogTypeEnricherProvider.cs
@@ -0,0 +1,27 @@
+﻿using Logging.Configuration.Enrichers;
+using Logging.Models.LoggingModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Logging.Configuration.Enrichers
+{
+    public class DefaultLogTypeEnricherProvider : ILogEventEnricher
+    {
+        private readonly DefaultLogTypeEnricher _enricher;
+
+        public DefaultLogTypeEnricherProvider(IServiceProvider serviceProvider)
+        {
+            var httpContextAccessor = serviceProvider.GetRequiredService<IHttpContextAccessor>();
+            _enricher = new DefaultLogTypeEnricher(httpContextAccessor);
+        }
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            _enricher.Enrich(logEvent, propertyFactory);
+        }
+    }
+}
+
+
