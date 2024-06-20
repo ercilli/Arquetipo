790c7b6ed488becc9d19ea33447113598f029f7c
ercilli <eric.tarelli@gmail.com>
2024-06-20 17:02:20 -0300

    feat: Add Logging.Configuration.Pom project reference and refactor code for HttpContext access

---
+++ Logging.Configuration.Pom/ConfigurationExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace Logging.Configuration.Pom;
+
+public static class AddLoggingPom
+{
+    public static LoggerConfiguration AddSerilogServicesPom()
+    {
+        return new LoggerConfiguration().Enrich.With(new EnricherPom());
+    }
+}
