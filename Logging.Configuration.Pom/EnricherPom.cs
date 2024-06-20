717fcfa7ebf9bc849437b23f852c6bb8402e2a72
ercilli <eric.tarelli@gmail.com>
2024-06-20 17:02:27 -0300

    Refactor code to use EnricherPom for adding MachineName property to log events

---
+++ Logging.Configuration.Pom/EnricherPom.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Logging.Configuration.Pom
+{
+    public class EnricherPom : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("MachineName", Environment.MachineName));
+        }
+    }
+}
+
