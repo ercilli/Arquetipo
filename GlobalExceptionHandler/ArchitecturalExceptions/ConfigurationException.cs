5340be9a076b66524791498a561159e037460070
ercilli <eric.tarelli@gmail.com>
2024-06-22 16:41:39 -0300

    Refactor ConfigurationException class and update namespace

--- GlobalExceptionHandler/ArchitecturalExceptions/ConfigurationException.cs
+++ GlobalExceptionHandler/ArchitecturalExceptions/ConfigurationException.cs
@@ -1,11 +1,21 @@
 ﻿using System;
-namespace Architectural.Exception.Core.ArchitecturalExceptions
+using GlobalExceptionHandler.Base;
+
+namespace GlobalExceptionHandler.Architectural.Configuration
 {
-	public class ConfigurationException
-	{
-		public ConfigurationException()
-		{
-		}
-	}
+    public class ConfigurationException : BaseException
+    {
+        public ConfigurationException()
+        {
+        }
+
+        public ConfigurationException(string message) : base(message)
+        {
+        }
+
+        public ConfigurationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
 }
 
