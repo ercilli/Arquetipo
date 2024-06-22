67eb31ae3fc62aa4f0aa90174b7397c95ecb4911
ercilli <eric.tarelli@gmail.com>
2024-06-22 16:40:29 -0300

    feat: Refactor DatabaseException class and update namespace

--- GlobalExceptionHandler/TechnicalExceptions/DatabaseException.cs
+++ GlobalExceptionHandler/TechnicalExceptions/DatabaseException.cs
@@ -1,11 +1,21 @@
 ﻿using System;
-namespace Architectural.Exception.Core.TechnicalExceptions
+using GlobalExceptionHandler.Base;
+
+namespace GlobalExceptionHandler.Technical.Database
 {
-	public class DatabaseException
-	{
-		public DatabaseException()
-		{
-		}
-	}
+    public class DatabaseException : BaseException
+    {
+        public DatabaseException()
+        {
+        }
+
+        public DatabaseException(string message) : base(message)
+        {
+        }
+
+        public DatabaseException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
 }
 
