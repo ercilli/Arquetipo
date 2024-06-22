ed30bfa17d3f5e30da9ce9709199e15ac37b55e6
ercilli <eric.tarelli@gmail.com>
2024-06-22 03:02:04 -0300

    feat: Add error handling for HttpContext exceptions

--- ErrorHandling/Abstracts/BaseExceptionHandler.cs
+++ ErrorHandling/Abstracts/BaseExceptionHandler.cs
@@ -1,40 +1,49 @@
 ﻿using System.Net;
-using System.Text.Json;
-using ErrorHandling.Interfaces;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using ResponseGenerator.Models.ResponseGeneratorModels;
+using ErrorHandling.Interfaces;
+using System.Collections.Generic; // Necesario para List<T>
 
 namespace ErrorHandling.Abstracts
 {
     public abstract class BaseExceptionHandler : IExceptionHandler
     {
-        private readonly ResponseApi _model;
+        protected readonly ResponseApi _model;
 
         protected BaseExceptionHandler(ResponseApi model)
         {
             _model = model;
+            // Asegurarse de que la lista de errores está inicializada
+            if (_model.ListErrors == null)
+            {
+                _model.ListErrors = new List<BaseErrorModel>();
+            }
         }
 
         public async Task HandleExceptionAsync(Exception exception, HttpContext context)
         {
             context.Response.ContentType = "application/json";
-            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError; 
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
             FillErrorAditional(exception, context);
-
             FillError(exception, context);
-
         }
 
         private void FillError(Exception ex, HttpContext context)
         {
-            _model.Errors.code = context.Response.StatusCode.ToString();
-            _model.Errors.data = "Parte del PaaS";
+            // Crear y configurar el nuevo error
+            var error = new BaseErrorModel
+            {
+                code = context.Response.StatusCode.ToString(),
+                data = "Parte del PaaS"
+            };
+
+            // Agregar el nuevo error a la lista
+            _model.ListErrors.Add(error);
         }
 
         protected abstract void FillErrorAditional(Exception ex, HttpContext httpContext);
-
         protected abstract Task FillErrorAditionalAsync(Exception ex, HttpContext httpContext);
     }
 }
-
