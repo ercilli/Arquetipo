71d14ab70073e61d535909c58813b5623216cbf8
ercilli <eric.tarelli@gmail.com>
2024-06-22 16:40:50 -0300

    feat: Refactor NotFoundException class and update namespace

--- GlobalExceptionHandler/FunctionalExceptions/NotFoundException.cs
+++ GlobalExceptionHandler/FunctionalExceptions/NotFoundException.cs
@@ -1,11 +1,21 @@
 ﻿using System;
-namespace Architectural.Exception.Core.FunctionalExceptions
+using GlobalExceptionHandler.Base;
+
+namespace GlobalExceptionHandler.Functional.NotFound
 {
-	public class NotFoundException
-	{
-		public NotFoundException()
-		{
-		}
-	}
+    public class NotFoundException : BaseException
+    {
+        public NotFoundException()
+        {
+        }
+
+        public NotFoundException(string message) : base(message)
+        {
+        }
+
+        public NotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
 }
 
