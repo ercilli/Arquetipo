9a2b03a690142825490d3b77cb0b141b9a4a4ef3
ercilli <eric.tarelli@gmail.com>
2024-06-30 20:18:21 -0300

    refactor: Add Swagger configuration extension for ASP.NET Core

---
+++ Swagger.Configuration/Extensions/SwaggerConfigurationExtension.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Swagger.Configuration.Extensions
+{
+    public static class SwaggerConfigurationExtensions
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            var title = Environment.GetEnvironmentVariable("API_TITLE") ?? Assembly.GetExecutingAssembly().GetName().Name;
+            var version = "v1";
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(version, new OpenApiInfo { Title = title, Version = version });
+
+                // Configuración para JWT en Swagger
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme.",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                });
+
+                // Configuración para personalizar operationId y otros aspectos
+                c.CustomOperationIds(apiDesc =>
+                {
+                    return apiDesc.TryGetMethodInfo(out MethodInfo methodInfo) ? methodInfo.Name : null;
+                });
+            });
+
+            return services;
+        }
+    }
+}
