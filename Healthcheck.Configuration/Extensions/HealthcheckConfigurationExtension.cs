0a0c10dc1bfcfe64544c7e0eeb7afa3a0bdf05b8
ercilli <eric.tarelli@gmail.com>
2024-06-30 20:18:37 -0300

    refactor: Add HealthcheckConfigurationExtension.cs for health check configuration extension

---
+++ Healthcheck.Configuration/Extensions/HealthcheckConfigurationExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Healthcheck.Configuration.Extensions
+{
+    public static class HealthcheckConfigurationExtension
+    {
+        public static IServiceCollection AddBaseHealthChecks(this IServiceCollection services)
+        {
+            // Agrega los health checks aquí
+            services.AddHealthChecks();
+
+            return services;
+        }
+    }
+}
+
