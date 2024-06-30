8862d94a76798c7d6f98514e316a5fcb1af0db16
ercilli <eric.tarelli@gmail.com>
2024-06-30 20:19:37 -0300

    feat: Add ConfigurationPaaSMiddlewares.cs for PaaS middleware configuration extension

---
+++ Arquetipo.Paas/Extensions/ConfigurationPaaSMiddlewares.cs
@@ -0,0 +1,35 @@
+﻿using Arquetipo.Paas.MiddlewareRegistration;
+using Microsoft.AspNetCore.Builder;
+using Cors.Configuration.Extensions;
+using Healthcheck.Configuration.Extensions;
+using Swagger.Configuration.Extensions;
+
+namespace Arquetipo.Paas.Extensions
+{
+	public static class ConfigurationPaaSMiddlewares
+	{
+        public static IApplicationBuilder UsePaas(this IApplicationBuilder app)
+        {
+            MiddlewareExtraPaaS(app);
+
+            var middlewareRegistry = new MiddlewareRegistry();
+
+            foreach (var middlewareType in middlewareRegistry.GetOrderedMiddlewareTypes())
+            {
+                app.UseMiddleware(middlewareType);
+            }
+
+            return app;
+        }
+
+        public static IApplicationBuilder MiddlewareExtraPaaS(this IApplicationBuilder app)
+        {
+            app.UseCorsConfiguration();
+            app.UseBaseHealthChecks();
+            app.UseSwaggerConfiguration();
+
+            return app;
+        }
+    }
+}
+
