69609d68b26f09442673b980cafd3972968a72f8
ercilli <eric.tarelli@gmail.com>
2024-06-30 20:19:22 -0300

    refactor: Add ConfigurationMiddlewarePOM.cs for POM configuration extension

--- Arquetipo.Pom/Extensions/ConfigurationMiddlewares.cs
+++ Arquetipo.Pom/Extensions/ConfigurationMiddlewares.cs
@@ -1,14 +1,16 @@
-﻿using System;
+﻿using Arquetipo.Paas.Extensions;
 using Arquetipo.Paas.MiddlewareRegistration;
 using Logging.Filter.Pom;
 using Microsoft.AspNetCore.Builder;
 
 namespace Arquetipo.Pom.Extensions
 {
-	public static class ConfigurationMiddlewares
+    public static class ConfigurationMiddlewares
 	{
         public static IApplicationBuilder UsePOM(this IApplicationBuilder app)
         {
+            ConfigurationPaaSMiddlewares.MiddlewareExtraPaaS(app);
+
             var middlewareRegistry = new MiddlewareRegistry();
 
             middlewareRegistry.RegisterMiddlewareAtPosition(typeof(MiddlewarePom), 0);
