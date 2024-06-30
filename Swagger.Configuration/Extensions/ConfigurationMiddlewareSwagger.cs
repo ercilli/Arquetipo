c87b0610542fe5c9b54ec2d6ca849f2fd50faa9d
ercilli <eric.tarelli@gmail.com>
2024-06-30 20:18:29 -0300

    refactor: Add ConfigurationMiddlewareSwagger.cs for Swagger configuration extension

---
+++ Swagger.Configuration/Extensions/ConfigurationMiddlewareSwagger.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Swagger.Configuration.Extensions
+{
+    public static class ConfigurationMiddlewareSwagger
+    {
+        public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            var env = app.ApplicationServices.GetRequiredService<IWebHostEnvironment>();
+
+            if (!env.IsProduction())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            return app;
+        }
+    }
+}
