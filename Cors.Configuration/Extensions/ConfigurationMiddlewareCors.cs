528d579d09134744cf26350fdf71fbd45e93444b
ercilli <eric.tarelli@gmail.com>
2024-06-30 20:19:18 -0300

    feat: Add ConfigurationMiddlewareCors for CORS configuration extension

---
+++ Cors.Configuration/Extensions/ConfigurationMiddlewareCors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+
+namespace Cors.Configuration.Extensions
+{
+    public static class ConfigurationMiddlewareCors
+    {
+        public static IApplicationBuilder UseCorsConfiguration(this IApplicationBuilder app)
+        {
+            app.UseCors();
+
+            return app;
+        }
+    }
+}
+
