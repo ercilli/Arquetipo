39a5de4db64309116b98447c91c6dd8d5764706f
ercilli <eric.tarelli@gmail.com>
2024-07-01 22:59:00 -0300

    refactor: Update ResponseApi class to include meta, data, and errors properties

--- ResponseGenerator.Models/ResponseGeneratorModels/ResponseApi.cs
+++ ResponseGenerator.Models/ResponseGeneratorModels/ResponseApi.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
 namespace ResponseGenerator.Models.ResponseGeneratorModels
 {
 	public class ResponseApi
 	{
+		[JsonPropertyName("meta")]
 		public Meta Meta { get; set; } = new Meta();
+
+		[JsonPropertyName("data")]
 		public string Data { get; set; }
+
+		[JsonPropertyName("errors")]
 		public List<BaseErrorModel> ListErrors { get; set; } = new List<BaseErrorModel>();
 	}
 }
-
