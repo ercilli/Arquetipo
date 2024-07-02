ce3b23c2e00af1334c9ee461931a6724e5732ece
ercilli <eric.tarelli@gmail.com>
2024-07-01 22:59:12 -0300

    refactor: Remove logging statements from MiddlewarePom.cs

--- Logging.Filter.Pom/MiddlewarePom.cs
+++ Logging.Filter.Pom/MiddlewarePom.cs
@@ -18,11 +18,7 @@ namespace Logging.Filter.Pom
 
         public async Task InvokeAsync(HttpContext context)
         {
-            _logger.LogInformation("Ingreso al middleware Filter POM");
-            Console.WriteLine("Ingreso al middleware Filter POM");
             await _next(context);
-            _logger.LogInformation("Salgo del middleware Filter POM");
-            Console.WriteLine("Salgo del middleware Filter POM");
         }
     }
 }
